f58c930c9079abed3da274fb50e0e4e2f6ee1595
jual ahmed <md.jual.ah@gmail.com>
2020-05-04 02:13:46 +0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+	<p>Home Page</p>
+</div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+export default {
+	data () {
+      return {
+      	banner:[],
+      	offerproduct:[],
+        notice:[]
+      }
+    },
+    computed:{
+        
+    },
+    methods:{
+    	
+    },
+	async asyncData ({ params }) {
+        let banner = await axios.get(`/api/banner/`)
+	    let notice = await axios.get(`/api/notice/`)
+	    let offerproduct = await axios.get(`/api/offerproduct/`)
+
+	   	return {
+	       banner: banner.data,
+           offerproduct: offerproduct.data,
+	       notice: notice.data,
+	    }
+  	}
+}
+</script>
+
