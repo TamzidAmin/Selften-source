66b5fa88b12c8ea6de84cb29dbe956803ec4a551
jual ahmed <md.jual.ah@gmail.com>
2020-05-10 02:41:56 +0600

    added gooogle addsend

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,21 @@
 		  	<a :href="b.link" target="_blank"><img :src="'https://admin.selften.com/uploads/banner/'+b.banner" alt="" class="mx-auto"/></a>
 		</div>
 	</siema>
+	<div>
+			
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- ads2 -->
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-2716727174821674"
+		     data-ad-slot="7639053074"
+		     data-ad-format="auto"
+		     data-full-width-responsive="true"></ins>
+		<script>
+		     (adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+
+	</div>
 </div>
 </template>
 
