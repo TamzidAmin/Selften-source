aa86339e14aa4c91e41939daec007c97b19ba64d
jual ahmed <md.jual.ah@gmail.com>
2020-05-10 02:36:38 +0600

    df

--- resources/plugins/scroll.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-import vuescroll from 'vuescroll/dist/vuescroll-native';
-
-Vue.use(vuescroll);
--- resources/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-import Vuetify from 'vuetify/lib'
-
-Vue.use(Vuetify)
-
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-  },
-})
