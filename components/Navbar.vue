bd59a8e9b72c4ec5c7f81724ddbd2c5c390318aa
jual ahmed <md.jual.ah@gmail.com>
2020-05-13 03:16:22 +0600

    change nav layut

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,8 @@
 <template>
 <div class="shadow bg-white">
-	<header class=" mx-auto container">
+	<header class="mx-auto container">
 		<div class="py-1">
-			<nav class="flex items-center justify-between flex-wrap">
-				<nuxt-link to="/">
-					<img src="~/assets/selften.png" alt="" class="w-40">
-				</nuxt-link>
+			<nav class="flex items-center justify-between">
 				<div class="block lg:hidden">
 					<button @click="toggle()"
 							class="navbar-burger flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
@@ -16,7 +13,11 @@
 						</svg>
 					</button>
 				</div>
-				<div id="main-nav" class="w-full flex-grow lg:flex items-center lg:w-auto hidden">
+				<nuxt-link to="/">
+					<img src="~/assets/selften.png" alt="" class="w-20 md:w-40">
+				</nuxt-link>
+				
+				<div class="w-full flex-grow lg:flex items-center lg:w-auto hidden">
 					<div class="text-sm lg:flex-grow mt-2 animated jackinthebox xl:mx-8">
 						<nuxt-link to="/playzone"
 							class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg">
@@ -39,61 +40,89 @@
 							CONTACT US
 						</a>
 					</div>
-					<div class="relative">
-						<div class="flex">
-							<template v-if="token"> 
-								<button id="userButton" class="flex items-center focus:outline-none mr-3 ml-3" @click="dropdown()">
-									<avatar :username="user.username" v-if="user" backgroundColor="#D81C4B" color="#fff" :size="30"></avatar>
-									<div v-if="user" class="flex"><span class="px-3">Hi, {{ user.username }}</span> <svg  aria-hidden="true" focusable="false" data-prefix="fad" data-icon="angle-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-5 h-5"><g class="fa-group"><path fill="currentColor" d="M160 256.14l-56.51 56.47-96.44-96.15a23.77 23.77 0 0 1-.18-33.61l.18-.18 22.59-22.51a23.94 23.94 0 0 1 33.85 0z" class="fa-secondary"></path><path fill="currentColor" d="M313 182.57L290.21 160a23.94 23.94 0 0 0-33.85 0L103.47 312.61 143 352l.06.06a24 24 0 0 0 33.93-.16L313 216.36l.18-.17a23.78 23.78 0 0 0-.18-33.62z" class="fa-primary"></path></g></svg></div> 
-								</button>
-							</template>
-							<template v-else>
-								<div class="p-3">
-									<nuxt-link class="bg-white hover:text-white hover:bg-green-100 text-green-800 font-semibold py-2 uppercase px-4 border border-green-100 rounded shadow" to="/login">
-										login
-									</nuxt-link>
-									<nuxt-link class="ml-3 bg-green-100 border border-green-100 hover:bg-green-200 text-white font-semibold py-2 px-4 rounded uppercase" to="/register">
-										register
-									</nuxt-link>
-								</div>
-							</template>
-						</div>
-						<div id="userMenu" v-if="token" v-bind:class="activeClass ? '' : 'hidden'"  class="bg-white rounded shadow-md mt-2 absolute mt-12 top-0 right-0 min-w-full overflow-auto z-30">
-							<ul class="list-reset w-40">
-								<li @click="dropdown()">
-									<router-link :to="/profile/+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
-										My Account
-									</router-link>
-								</li>
-								<li @click="dropdown()">
-									<router-link :to="'/profile/order/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
-										My Order
-									</router-link>
-								</li>
-								<li @click="dropdown()">
-									<router-link :to="'/profile/wallet/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
-										My wallet
-									</router-link>
-								</li>
-								<li @click="dropdown()">
-									<router-link :to="'/profile/settings/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
-										Settings
-									</router-link>
-								</li>
-								<li>
-									<hr class="border-t mx-2 border-gray-400">
-								</li>
-								<li>
-									<a href="#" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline" @click="logout">
-										Logout
-									</a>
-								</li>
-							</ul>
-						</div>
+				</div>
+
+				<div class="relative">
+					<div class="flex">
+						<template v-if="token"> 
+							<button id="userButton" class="flex items-center focus:outline-none mr-3 ml-3" @click="dropdown()">
+								<avatar :username="user.username" v-if="user" backgroundColor="#D81C4B" color="#fff" :size="30"></avatar>
+								<div v-if="user" class="flex"><span class="px-3">Hi, {{ user.username }}</span> <svg  aria-hidden="true" focusable="false" data-prefix="fad" data-icon="angle-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-5 h-5"><g class="fa-group"><path fill="currentColor" d="M160 256.14l-56.51 56.47-96.44-96.15a23.77 23.77 0 0 1-.18-33.61l.18-.18 22.59-22.51a23.94 23.94 0 0 1 33.85 0z" class="fa-secondary"></path><path fill="currentColor" d="M313 182.57L290.21 160a23.94 23.94 0 0 0-33.85 0L103.47 312.61 143 352l.06.06a24 24 0 0 0 33.93-.16L313 216.36l.18-.17a23.78 23.78 0 0 0-.18-33.62z" class="fa-primary"></path></g></svg></div> 
+							</button>
+						</template>
+						<template v-else>
+							<div class="p-3">
+								<nuxt-link class="bg-white hover:text-white hover:bg-green-100 text-green-800 font-semibold py-2 uppercase px-4 border border-green-100 rounded shadow" to="/login">
+									login
+								</nuxt-link>
+								<nuxt-link class="ml-3 bg-green-100 border border-green-100 hover:bg-green-200 text-white font-semibold py-2 px-4 rounded uppercase" to="/register">
+									register
+								</nuxt-link>
+							</div>
+						</template>
+					</div>
+					<div id="userMenu" v-if="token" v-bind:class="activeClass ? '' : 'hidden'"  class="bg-white rounded shadow-md mt-2 absolute mt-12 top-0 right-0 min-w-full overflow-auto z-30">
+						<ul class="list-reset w-40">
+							<li @click="dropdown()">
+								<router-link :to="/profile/+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
+									My Account
+								</router-link>
+							</li>
+							<li @click="dropdown()">
+								<router-link :to="'/profile/order/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
+									My Order
+								</router-link>
+							</li>
+							<li @click="dropdown()">
+								<router-link :to="'/profile/wallet/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
+									My wallet
+								</router-link>
+							</li>
+							<li @click="dropdown()">
+								<router-link :to="'/profile/settings/'+user.id" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline">
+									Settings
+								</router-link>
+							</li>
+							<li>
+								<hr class="border-t mx-2 border-gray-400">
+							</li>
+							<li>
+								<a href="#" class="px-4 py-2 block text-gray-900 hover:bg-green-400 hover:text-white no-underline hover:no-underline" @click="logout">
+									Logout
+								</a>
+							</li>
+						</ul>
 					</div>
 				</div>
+
 			</nav>
 		</div>
+		
+		<div id="main-nav" class="w-full lg:w-auto hidden absolute left-0 bg-white z-20">
+			<div class="text-sm lg:flex-grow mt-2 animated jackinthebox xl:mx-8">
+				<nuxt-link to="/playzone" @click.native="remove()"
+					class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg">
+					PLAYZONE
+				</nuxt-link>
+				<nuxt-link to="/topup" @click.native="remove()"
+					class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg">
+					TOP-UP
+				</nuxt-link>
+				<nuxt-link to="/shop" @click.native="remove()"
+					class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg">
+					SHOP
+				</nuxt-link>
+				<nuxt-link to="/leaderboard" @click.native="remove()"
+					class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg uppercase">
+					leaderboard
+				</nuxt-link>
+				<a href="https://t.me/joinchat/Q77WBk4Jli5iqWsgJqatKw" target="_blank" @click.native="remove()"
+					class="block lg:inline-block text-md font-bold hover:text-red-900 mx-2 p-1 rounded-lg">
+					CONTACT US
+				</a>
+			</div>
+		</div>
+
 	</header>
 </div>
 </template>
@@ -122,6 +151,11 @@
 			toggle(){
 				var $target = document.getElementById('main-nav');
 				$target.classList.toggle('hidden');
+			},
+			remove(){
+				console.log("aaaaaaaaaaaaaa");
+				var $target = document.getElementById('main-nav');
+				$target.classList.add('hidden');
 			},
 			async logout () {
 				this.activeClass=!this.activeClass
