cc1c35a9d81f41b7367e474e2e1b96b5490729bd
jual ahmed <md.jual.ah@gmail.com>
2020-05-17 18:50:26 +0600

    sdf

--- pages/oauth/_id.vue
+++ pages/oauth/_id.vue
@@ -1,36 +1,42 @@
 <template>
-	<p>Lgoin</p>
+	<p>Lgoinas {{ users() }}</p>
 </template>
 <script>
 import { required } from 'vuelidate/lib/validators'
 import axios from '~/plugins/axios'
 const Cookie = process.client ? require('js-cookie') : undefined
 export default{
+	data: () => ({
+		id: 0,
+	}),
+	methods: {
+		users(){
+			 	var self = this;
+				axios.post('/api/loginbyid/'+this.id)
+				.then(function (response) {
+					console.log(response.data);
+					setTimeout(() => { // we simulate the async request with timeout.
+					const auth = {
+					  accessToken: response.data.token
+					}
+					console.log(auth);
+					self.$store.commit('setToken', auth) // mutating to store for client rendering
+					self.$store.commit('setUser', response.data) // mutating to store for client rendering
+					Cookie.set('token', auth,{ expires: 365 }) // saving token in cookie for server rendering
+					Cookie.set('user', response.data,{ expires: 365 }) // saving token in cookie for server rendering
+					window.close();
+				  }, 1000)
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		}
+	},
 	async asyncData ({ params }) {
-		axios.post('/api/loginbyid/'+params.id)
-		.then(function (response) {
-			console.log(response.data);
-			if(response.data.message){
-				self.loading=false
-				self.error=response.data.message
-			}else{
-				self.loading=false
-				setTimeout(() => { // we simulate the async request with timeout.
-				const auth = {
-				  accessToken: response.data.token
-				}
-				console.log(auth);
-				self.$store.commit('setToken', auth) // mutating to store for client rendering
-				self.$store.commit('setUser', response.data) // mutating to store for client rendering
-				Cookie.set('token', auth,{ expires: 365 }) // saving token in cookie for server rendering
-				Cookie.set('user', response.data,{ expires: 365 }) // saving token in cookie for server rendering
-				window.close();
-			  }, 1000)
-			}
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+		return {
+			id:params.id
+		}
+
 	}
 }
 </script>
