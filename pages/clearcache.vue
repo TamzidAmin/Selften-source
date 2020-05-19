2c107909708e2f997caed3577701f167bcc8d5cf
jual ahmed <md.jual.ah@gmail.com>
2020-05-19 09:59:52 +0600

    clear cache

---
+++ pages/clearcache.vue
@@ -0,0 +1,18 @@
+<template>
+	<p>aaaaaaaaa {{ logout() }}</p>
+</template>
+
+<script>
+	const Cookie = process.client ? require('js-cookie') : undefined
+	export default {
+		methods: {
+			async logout () {
+				Cookie.remove('token')
+		      	Cookie.remove('setUser')
+		      	this.$store.commit('setToken', null)
+		      	this.$store.commit('setUser', null)
+				this.$router.push('/')
+			},
+		}
+	}
+</script>
