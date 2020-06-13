a3564fedb2a06e87f3aa75eea48a6f23f77d01fe
Jual Ahmed <md.jual.ah@gmail.com>
2020-06-13 13:37:33 +0600

    design some home pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="bg-gray-100">
 	<div class="min-h">
+		<div> <span class="hidden">{{ getnotice }}</span>
+			<center v-if="notice[0]">
+				<div><marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();" id="MARQUEE1" class="scrolling">
+					<b class="text-info">NOTICE : <span class="text-green-100" v-if="notice[0]"> {{ notice[0].notice }}</span></b></marquee>
+				</div>
+			</center>
+		</div>
 		<Navbar/>
 		<div class="mx-auto">
 			<nuxt />
@@ -16,10 +23,23 @@
 <script>
 	import Navbar from '~/components/Navbar.vue'
 	import Footer from '~/components/Footer.vue'
+	import axios from '~/plugins/axios'
 	export default{
+		data () {
+			return {
+				notice:[]
+			}
+		},
 		components: {
 			Navbar,
 			Footer,
+		},
+		computed:{
+			async getnotice(){
+				let notice = await axios.get(`/api/notice/`)
+				console.log(notice)
+				this.notice=notice.data
+			}
 		},
 	}
 </script>
--- plugins/common.js
+++ plugins/common.js
@@ -3,6 +3,9 @@ import Siema from 'vue2-siema'
 import ProgressBar from 'vuejs-progress-bar'
 import VueLoading from 'vuejs-loading-plugin'
 
+import Carousel3d from 'vue-carousel-3d';
+Vue.use(Carousel3d);
+
 Vue.use(VueLoading)
 
 Vue.use(ProgressBar)
